4cafbc50d31056d01e123934a243eb5624d0f5b9
artem.novikov <biokako@gmail.com>
2022-12-26 17:29:33 +0400

    using standard problemdetail

--- BuberDinner.Api/Filters/ErrorHandlingFilterAttribute.cs
+++ BuberDinner.Api/Filters/ErrorHandlingFilterAttribute.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
@@ -8,11 +9,15 @@ public class ErrorHandlingFilterAttribute : ExceptionFilterAttribute
     public override void OnException(ExceptionContext context)
     {
         var exception = context.Exception;
-        var errorResult = new { error = "An error occurred while processing your request." };
-        context.Result = new ObjectResult(errorResult)
-        {
-            StatusCode = 500
+        var problemDetails = new ProblemDetails{
+            Type = "https://www.rfc-editor.org/rfc/rfc7231#section-6.6.1",
+            Instance = context.HttpContext.Request.Path,
+            Status = (int)HttpStatusCode.InternalServerError,
+            Title = "An error occurred while processing your request.",
+            Detail = exception.Message
         };
+
+        context.Result = new ObjectResult(problemDetails);
         context.ExceptionHandled = true;
     }
 }
