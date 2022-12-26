655bcab0d9344e49ff5eb40471d81b78c7396e33
artem.novikov <biokako@gmail.com>
2022-12-26 18:32:39 +0400

    altering problemdetailsfactory

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,7 +1,9 @@
+using BuberDinner.Api.Errors;
 using BuberDinner.Api.Filters;
 using BuberDinner.Api.Middleware;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -11,6 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 
     // builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
     builder.Services.AddControllers();
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
