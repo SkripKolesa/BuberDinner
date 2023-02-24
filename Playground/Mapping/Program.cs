4190a8eba78e07581a7b9569f7f1fc7bf090e0df
artem.novikov <biokako@gmail.com>
2023-02-24 14:39:53 +0400

    global config + ignore non mapped

--- Playground/Mapping/Program.cs
+++ Playground/Mapping/Program.cs
@@ -6,9 +6,10 @@ internal class Program
     private static void Main(string[] args)
     {
         var user = UserGenerator.GenerateRandom();
-        var config = new TypeAdapterConfig();
+        var config = TypeAdapterConfig.GlobalSettings;
         config.NewConfig<User, UserResponse>()
-            .Map(dest => dest.FullName, src => $"{src.FirstName} {src.LastName}");
+            .Map(dest => dest.FullName, src => $"{src.FirstName} {src.LastName}")
+            .IgnoreNonMapped(true);
         var userResponse = user.Adapt<UserResponse>(config);
         Console.WriteLine(user);
         Console.WriteLine(userResponse);
