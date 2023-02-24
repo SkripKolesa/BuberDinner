97f17d3854719a6bd8339f0660ab2049b8a8248b
artem.novikov <biokako@gmail.com>
2023-02-24 14:42:13 +0400

    appending config using ForType

--- Playground/Mapping/Program.cs
+++ Playground/Mapping/Program.cs
@@ -6,11 +6,13 @@ internal class Program
     private static void Main(string[] args)
     {
         var user = UserGenerator.GenerateRandom();
-        var config = TypeAdapterConfig.GlobalSettings;
-        config.NewConfig<User, UserResponse>()
-            .Map(dest => dest.FullName, src => $"{src.FirstName} {src.LastName}")
-            .IgnoreNonMapped(true);
-        var userResponse = user.Adapt<UserResponse>(config);
+        TypeAdapterConfig.GlobalSettings.NewConfig<User, UserResponse>()
+            .Map(dest => dest.FullName, src => $"{src.FirstName} {src.LastName}");
+
+        TypeAdapterConfig.GlobalSettings.ForType<User, UserResponse>()
+            .Map(dest => dest.Id, src => src.Id + 1);
+
+        var userResponse = user.Adapt<UserResponse>();
         Console.WriteLine(user);
         Console.WriteLine(userResponse);
     }
